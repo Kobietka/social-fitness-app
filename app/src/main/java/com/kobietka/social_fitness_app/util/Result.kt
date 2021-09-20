b1fc01923aaaa01fc96c5296152ced80dd7f979b
kobietka <slawekgrawgry@gmail.com>
2021-09-20 15:48:25 +0200

    Add Result sealed class

---
+++ app/src/main/java/com/kobietka/social_fitness_app/util/Result.kt
@@ -0,0 +1,7 @@
+package com.kobietka.social_fitness_app.util
+
+
+sealed class Result<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T): Result<T>(data = data)
+    class Failure<T>(message: String): Result<T>(message = message)
+}
