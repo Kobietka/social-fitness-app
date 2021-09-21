1bac654a53894cd58831e578bd37902ff242e28e
kobietka <slawekgrawgry@gmail.com>
2021-09-21 11:06:17 +0200

    Add navigation to MainActivity

--- app/src/main/java/com/kobietka/social_fitness_app/presentation/MainActivity.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/presentation/MainActivity.kt
@@ -3,10 +3,10 @@ package com.kobietka.social_fitness_app.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.kobietka.social_fitness_app.presentation.login.LoginScreen
 import com.kobietka.social_fitness_app.presentation.register.RegisterScreen
 import com.kobietka.social_fitness_app.presentation.theme.SocialfitnessappTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,8 +16,21 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             SocialfitnessappTheme {
-                RegisterScreen()
+                NavHost(navController = navController, startDestination = Screen.Login.route){
+                    composable(route = Screen.Login.route){
+                        LoginScreen(navController = navController)
+                    }
+
+                    composable(route = Screen.Register.route){
+                        RegisterScreen(navController = navController)
+                    }
+
+                    composable(route = Screen.MainScreen.route){
+
+                    }
+                }
             }
         }
     }
