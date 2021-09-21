61176e0e368dfedc42749e2fd3476405545abdba
kobietka <slawekgrawgry@gmail.com>
2021-09-21 11:02:00 +0200

    Add DatabaseModule

---
+++ app/src/main/java/com/kobietka/social_fitness_app/di/DatabaseModule.kt
@@ -0,0 +1,35 @@
+package com.kobietka.social_fitness_app.di
+
+import android.content.Context
+import androidx.room.Room
+import com.kobietka.social_fitness_app.data.AppDatabase
+import com.kobietka.social_fitness_app.data.dao.UserCredentialsDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "app_database"
+        ).fallbackToDestructiveMigration().build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserCredentialsDao(appDatabase: AppDatabase): UserCredentialsDao {
+        return appDatabase.userCredentialsDao()
+    }
+
+}
