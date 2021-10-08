59e068e195e1f2ea9904045bb2360c4d81270fe0
kobietka <slawekgrawgry@gmail.com>
2021-10-08 10:13:51 +0200

    Change endpoint

--- app/src/main/java/com/kobietka/social_fitness_app/domain/service/AuthService.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/domain/service/AuthService.kt
@@ -3,15 +3,14 @@ package com.kobietka.social_fitness_app.domain.service
 import com.kobietka.social_fitness_app.network.request.LoginUserRequest
 import com.kobietka.social_fitness_app.network.request.RegisterUserRequest
 import com.kobietka.social_fitness_app.network.response.LoginUserResponse
-import com.kobietka.social_fitness_app.network.response.RegisterUserResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 
 interface AuthService {
 
-    @POST("/api/register")
-    suspend fun registerUser(@Body registerUserRequest: RegisterUserRequest): RegisterUserResponse
+    @POST("/api/user/register")
+    suspend fun registerUser(@Body registerUserRequest: RegisterUserRequest)
 
     @POST("/api/login")
     suspend fun loginUser(@Body loginUserRequest: LoginUserRequest): LoginUserResponse
