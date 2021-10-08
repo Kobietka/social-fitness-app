437857a7dbdeb3eb1c989faea5a88246a2c8903a
kobietka <slawekgrawgry@gmail.com>
2021-10-08 10:59:05 +0200

    Add MainScreenState data class

---
+++ app/src/main/java/com/kobietka/social_fitness_app/presentation/main_screen/MainScreenState.kt
@@ -0,0 +1,7 @@
+package com.kobietka.social_fitness_app.presentation.main_screen
+
+import com.kobietka.social_fitness_app.domain.model.User
+
+data class MainScreenState(
+    val user: User = User(id = "", nickname = "")
+)
