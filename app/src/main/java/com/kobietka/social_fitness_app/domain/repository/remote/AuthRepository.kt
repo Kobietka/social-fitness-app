8645b16a6af15c355ea1e4f7684f74a8ce325ce9
kobietka <slawekgrawgry@gmail.com>
2021-10-08 10:15:26 +0200

    Change return type of registerUser method

--- app/src/main/java/com/kobietka/social_fitness_app/domain/repository/remote/AuthRepository.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/domain/repository/remote/AuthRepository.kt
@@ -3,12 +3,10 @@ package com.kobietka.social_fitness_app.domain.repository.remote
 import com.kobietka.social_fitness_app.network.request.LoginUserRequest
 import com.kobietka.social_fitness_app.network.request.RegisterUserRequest
 import com.kobietka.social_fitness_app.network.response.LoginUserResponse
-import com.kobietka.social_fitness_app.network.response.RegisterUserResponse
 import com.kobietka.social_fitness_app.util.Result
-import kotlinx.coroutines.flow.Flow
 
 
 interface AuthRepository {
-    suspend fun registerUser(registerUserRequest: RegisterUserRequest): Result<RegisterUserResponse>
+    suspend fun registerUser(registerUserRequest: RegisterUserRequest): Result<Boolean>
     suspend fun loginUser(loginUserRequest: LoginUserRequest): Result<LoginUserResponse>
 }
--- app/src/main/java/com/kobietka/social_fitness_app/network/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/network/repository/AuthRepositoryImpl.kt
@@ -1,14 +1,14 @@
 package com.kobietka.social_fitness_app.network.repository
 
+import com.google.gson.Gson
 import com.kobietka.social_fitness_app.domain.repository.remote.AuthRepository
 import com.kobietka.social_fitness_app.domain.service.AuthService
 import com.kobietka.social_fitness_app.network.request.LoginUserRequest
 import com.kobietka.social_fitness_app.network.request.RegisterUserRequest
+import com.kobietka.social_fitness_app.network.response.ErrorResponse
 import com.kobietka.social_fitness_app.network.response.LoginUserResponse
-import com.kobietka.social_fitness_app.network.response.RegisterUserResponse
+import com.kobietka.social_fitness_app.network.response.RegisterUserErrorResponse
 import com.kobietka.social_fitness_app.util.Result
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
 import java.io.IOException
 import javax.inject.Inject
@@ -16,14 +16,28 @@ import javax.inject.Inject
 
 class AuthRepositoryImpl
 @Inject constructor(private val authService: AuthService): AuthRepository {
-    override suspend fun registerUser(registerUserRequest: RegisterUserRequest): Result<RegisterUserResponse> {
+    override suspend fun registerUser(registerUserRequest: RegisterUserRequest): Result<Boolean> {
         return try {
-            val response = authService.registerUser(registerUserRequest = registerUserRequest)
-            Result.Success(data = response)
+            authService.registerUser(registerUserRequest = registerUserRequest)
+            Result.Success(data = true)
         } catch (exception: IOException){
             Result.Failure(message = "Cannot connect. Check your internet connection.")
         } catch (exception: HttpException){
-            Result.Failure(message = "Something went wrong. Try again later.")
+            val responseString = exception.response()?.errorBody()?.string()
+            return when(exception.code()){
+                422 -> {
+                    val errorResponse = Gson().fromJson(responseString, RegisterUserErrorResponse::class.java)
+                    val message = errorResponse.violations.foldRight(initial = ""){ violation, acc ->
+                        acc + violation.message + "\n"
+                    }
+                    Result.Failure(
+                        message = message
+                    )
+                }
+                else -> {
+                    Result.Failure(message = "Something went wrong. Try again later.")
+                }
+            }
         }
     }
 
@@ -34,7 +48,27 @@ class AuthRepositoryImpl
         } catch (exception: IOException){
             Result.Failure(message = "Cannot connect. Check your internet connection.")
         } catch (exception: HttpException){
-            Result.Failure(message = "Something went wrong. Try again later.")
+            val responseString = exception.response()?.errorBody()?.string()
+            val errorResponse = Gson().fromJson(responseString, ErrorResponse::class.java)
+            Result.Failure(message = errorResponse.message)
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
