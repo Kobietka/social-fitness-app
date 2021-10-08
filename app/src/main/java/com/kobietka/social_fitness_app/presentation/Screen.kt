7418d2c8ec1d0fde052e6a85f13f29aaec5f971e
kobietka <slawekgrawgry@gmail.com>
2021-10-08 11:32:17 +0200

    Add new Screen

--- app/src/main/java/com/kobietka/social_fitness_app/presentation/Screen.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/presentation/Screen.kt
@@ -5,4 +5,5 @@ sealed class Screen(val route: String){
     object Register : Screen("/register")
     object Login : Screen("/login")
     object MainScreen : Screen("/main")
+    object EditUser : Screen("/edit_user")
 }
