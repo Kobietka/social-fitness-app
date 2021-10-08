9a1a7f0dad8047ae51414d54248d64951d2313ef
kobietka <slawekgrawgry@gmail.com>
2021-10-08 10:59:24 +0200

    Add MainModule class

---
+++ app/src/main/java/com/kobietka/social_fitness_app/di/MainModule.kt
@@ -0,0 +1,22 @@
+package com.kobietka.social_fitness_app.di
+
+import com.kobietka.social_fitness_app.domain.repository.local.UserCredentialsRepository
+import com.kobietka.social_fitness_app.domain.usecase.main.GetUsersUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class MainModule {
+
+    @Provides
+    fun provideGetUsersUseCase(
+        userCredentialsRepository: UserCredentialsRepository
+    ): GetUsersUseCase {
+        return GetUsersUseCase(userCredentialsRepository = userCredentialsRepository)
+    }
+
+}
