dee8c8db1f6c58afb0a8ee72b91ae93f139a2443
kobietka <slawekgrawgry@gmail.com>
2021-10-08 10:14:44 +0200

    Add onConflict strategy to UserCredentialsDao

--- app/src/main/java/com/kobietka/social_fitness_app/data/dao/UserCredentialsDao.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/data/dao/UserCredentialsDao.kt
@@ -2,15 +2,17 @@ package com.kobietka.social_fitness_app.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
 import com.kobietka.social_fitness_app.data.entity.UserCredentialsEntity
 
 @Dao
 interface UserCredentialsDao {
 
-    @Insert
+    @Insert(onConflict = REPLACE)
     suspend fun insert(userCredentialsEntity: UserCredentialsEntity)
 
-    @Query("delete from userCredentials")
+    @Query("delete from user")
     suspend fun deleteAllCredentials()
+
 }
