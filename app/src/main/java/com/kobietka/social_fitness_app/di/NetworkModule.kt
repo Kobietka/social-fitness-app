9cfac60bb516f61d95ae5ae1868e230d0a1495cd
kobietka <slawekgrawgry@gmail.com>
2021-10-08 12:32:37 +0200

    Add dependencies to NetworkModule

--- app/src/main/java/com/kobietka/social_fitness_app/di/NetworkModule.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/di/NetworkModule.kt
@@ -1,13 +1,19 @@
 package com.kobietka.social_fitness_app.di
 
 import com.google.gson.Gson
+import com.kobietka.social_fitness_app.data.dao.UserCredentialsDao
 import com.kobietka.social_fitness_app.domain.service.AuthService
+import com.kobietka.social_fitness_app.domain.service.UpdateUserService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.runBlocking
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
 import javax.inject.Singleton
 
 
@@ -17,6 +23,7 @@ class NetworkModule {
 
     @Provides
     @Singleton
+    @Named("retrofit")
     fun provideRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://filkur-fitness-app.herokuapp.com")
@@ -26,8 +33,66 @@ class NetworkModule {
 
     @Provides
     @Singleton
-    fun provideAuthService(retrofit: Retrofit): AuthService {
+    @Named("retrofitAuth")
+    fun provideAuthRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://filkur-fitness-app.herokuapp.com")
+            .addConverterFactory(GsonConverterFactory.create(Gson()))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(@Named("token") token: String): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val original = chain.request()
+                val newRequest = original.newBuilder()
+                    .addHeader("Authorization", "Bearer $token")
+                    .method(original.method(), original.body())
+                    .url(original.url())
+                    .build()
+                chain.proceed(newRequest)
+            }.build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthService(@Named("retrofit") retrofit: Retrofit): AuthService {
         return retrofit.create(AuthService::class.java)
     }
 
+    @Provides
+    @Singleton
+    fun provideUpdateUserService(@Named("retrofitAuth") retrofit: Retrofit): UpdateUserService {
+        return retrofit.create(UpdateUserService::class.java)
+    }
+
+    @Provides
+    @Named("token")
+    fun provideUserToken(userCredentialsDao: UserCredentialsDao): String = runBlocking {
+        val loggedUser = userCredentialsDao.getAllUsers().toList().first()
+        return@runBlocking loggedUser.first().nickname
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
