7af2f0ac636af98497074ce5e093fbdf7b2a697e
kobietka <slawekgrawgry@gmail.com>
2021-10-09 12:30:19 +0200

    Add updateUserData method to UpdateUserService

--- app/src/main/java/com/kobietka/social_fitness_app/domain/service/UpdateUserService.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/domain/service/UpdateUserService.kt
@@ -1,7 +1,10 @@
 package com.kobietka.social_fitness_app.domain.service
 
+import com.kobietka.social_fitness_app.network.request.UpdateUserDataRequest
 import com.kobietka.social_fitness_app.network.request.UpdateUserPasswordRequest
+import com.kobietka.social_fitness_app.network.response.UpdateUserDataResponse
 import retrofit2.http.Body
+import retrofit2.http.PATCH
 import retrofit2.http.PUT
 import retrofit2.http.Path
 
@@ -9,9 +12,15 @@ import retrofit2.http.Path
 interface UpdateUserService {
 
     @PUT("/api/user/{userId}")
-    suspend fun updateUserData(
+    suspend fun updateUserPassword(
         @Path("userId") userId: String,
         @Body updateUserPasswordRequest: UpdateUserPasswordRequest
     )
+
+    @PATCH("/api/user/{userId}")
+    suspend fun updateUserData(
+        @Path("userId") userId: String,
+        @Body updateUserDataRequest: UpdateUserDataRequest
+    ): UpdateUserDataResponse
 
 }
