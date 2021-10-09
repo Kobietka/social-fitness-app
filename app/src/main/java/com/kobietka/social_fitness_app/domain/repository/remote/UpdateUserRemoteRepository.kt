a1f17c95d5dccd2ddb69038f0318f463538cd97f
kobietka <slawekgrawgry@gmail.com>
2021-10-09 12:32:59 +0200

    Add updateUserData method to UpdateUserRemoteRepository

--- app/src/main/java/com/kobietka/social_fitness_app/domain/repository/remote/UpdateUserRemoteRepository.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/domain/repository/remote/UpdateUserRemoteRepository.kt
@@ -1,6 +1,8 @@
 package com.kobietka.social_fitness_app.domain.repository.remote
 
+import com.kobietka.social_fitness_app.network.request.UpdateUserDataRequest
 import com.kobietka.social_fitness_app.network.request.UpdateUserPasswordRequest
+import com.kobietka.social_fitness_app.network.response.UpdateUserDataResponse
 import com.kobietka.social_fitness_app.util.Result
 
 
@@ -9,4 +11,8 @@ interface UpdateUserRemoteRepository {
         userId: String,
         updateUserPasswordRequest: UpdateUserPasswordRequest
     ): Result<Boolean>
+    suspend fun updateUserData(
+        userId: String,
+        updateUserDataRequest: UpdateUserDataRequest
+    ): Result<UpdateUserDataResponse>
 }
--- app/src/main/java/com/kobietka/social_fitness_app/network/repository/UpdateUserRemoteRepositoryImpl.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/network/repository/UpdateUserRemoteRepositoryImpl.kt
@@ -3,8 +3,10 @@ package com.kobietka.social_fitness_app.network.repository
 import com.google.gson.Gson
 import com.kobietka.social_fitness_app.domain.repository.remote.UpdateUserRemoteRepository
 import com.kobietka.social_fitness_app.domain.service.UpdateUserService
+import com.kobietka.social_fitness_app.network.request.UpdateUserDataRequest
 import com.kobietka.social_fitness_app.network.request.UpdateUserPasswordRequest
 import com.kobietka.social_fitness_app.network.response.RegisterUserErrorResponse
+import com.kobietka.social_fitness_app.network.response.UpdateUserDataResponse
 import com.kobietka.social_fitness_app.util.Result
 import retrofit2.HttpException
 import java.io.IOException
@@ -18,7 +20,7 @@ class UpdateUserRemoteRepositoryImpl(
         updateUserPasswordRequest: UpdateUserPasswordRequest
     ): Result<Boolean> {
         return try {
-            updateUserService.updateUserData(
+            updateUserService.updateUserPassword(
                 userId = userId,
                 updateUserPasswordRequest = updateUserPasswordRequest
             )
@@ -28,14 +30,47 @@ class UpdateUserRemoteRepositoryImpl(
         } catch (exception: HttpException){
             val responseString = exception.response()?.errorBody()?.string()
             return when(exception.code()){
+                401 -> {
+                    Result.Unauthorized()
+                }
                 422 -> {
                     val errorResponse = Gson().fromJson(responseString, RegisterUserErrorResponse::class.java)
                     val message = errorResponse.violations.foldRight(initial = ""){ violation, acc ->
                         acc + violation.message + "\n"
                     }
-                    Result.Failure(
-                        message = message
-                    )
+                    Result.Failure(message = message)
+                }
+                else -> {
+                    Result.Failure(message = "Something went wrong. Try again later.")
+                }
+            }
+        }
+    }
+
+    override suspend fun updateUserData(
+        userId: String,
+        updateUserDataRequest: UpdateUserDataRequest
+    ): Result<UpdateUserDataResponse> {
+        return try {
+            val response = updateUserService.updateUserData(
+                userId = userId,
+                updateUserDataRequest = updateUserDataRequest
+            )
+            Result.Success(data = response)
+        } catch (exception: IOException){
+            Result.Failure(message = "Cannot connect. Check your internet connection.")
+        } catch (exception: HttpException){
+            val responseString = exception.response()?.errorBody()?.string()
+            return when(exception.code()){
+                401 -> {
+                    Result.Unauthorized()
+                }
+                422 -> {
+                    val errorResponse = Gson().fromJson(responseString, RegisterUserErrorResponse::class.java)
+                    val message = errorResponse.violations.foldRight(initial = ""){ violation, acc ->
+                        acc + violation.message + "\n"
+                    }
+                    Result.Failure(message = message)
                 }
                 else -> {
                     Result.Failure(message = "Something went wrong. Try again later.")
@@ -44,3 +79,22 @@ class UpdateUserRemoteRepositoryImpl(
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
