1bac4f5a10fa5d98fc976026557ff66f30785c13
kobietka <slawekgrawgry@gmail.com>
2021-10-09 12:29:19 +0200

    Add Unauthorized to Result and Resource

--- app/src/main/java/com/kobietka/social_fitness_app/util/Result.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/util/Result.kt
@@ -4,4 +4,5 @@ package com.kobietka.social_fitness_app.util
 sealed class Result<T>(val data: T? = null, val message: String? = null) {
     class Success<T>(data: T): Result<T>(data = data)
     class Failure<T>(message: String): Result<T>(message = message)
+    class Unauthorized<T> : Result<T>()
 }
