ced92a12c1c19d12cbec5a3e8678be3f0dbb156f
kobietka <slawekgrawgry@gmail.com>
2021-10-13 15:51:32 +0200

    Add new fields to MainScreenState

--- app/src/main/java/com/kobietka/social_fitness_app/presentation/main_screen/MainScreenState.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/presentation/main_screen/MainScreenState.kt
@@ -3,5 +3,8 @@ package com.kobietka.social_fitness_app.presentation.main_screen
 import com.kobietka.social_fitness_app.domain.model.User
 
 data class MainScreenState(
-    val user: User = User(id = "", nickname = "", email = "")
+    val user: User = User(id = "", nickname = "", email = ""),
+    val isCreatingGroup: Boolean = false,
+    val isLoading: Boolean = false,
+    val error: String = ""
 )
