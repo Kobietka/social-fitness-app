af64b768b97174ba6ea67564b83a67dc0d2216a4
kobietka <slawekgrawgry@gmail.com>
2021-10-13 15:47:54 +0200

    Add GetGroupResponse data class

---
+++ app/src/main/java/com/kobietka/social_fitness_app/network/response/GetGroupResponse.kt
@@ -0,0 +1,13 @@
+package com.kobietka.social_fitness_app.network.response
+
+import com.kobietka.social_fitness_app.domain.model.User
+
+
+data class GetGroupResponse(
+    val id: String,
+    val name: String,
+    val description: String,
+    val owner: User,
+    val invitation: Invitation,
+    val members: List<MemberDto>
+)
