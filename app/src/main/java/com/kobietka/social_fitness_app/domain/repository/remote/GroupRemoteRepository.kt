19cf6603b2e51d629956798d305a571a6adbbe96
kobietka <slawekgrawgry@gmail.com>
2021-10-13 15:50:14 +0200

    Add GroupRemoteRepository

---
+++ app/src/main/java/com/kobietka/social_fitness_app/domain/repository/remote/GroupRemoteRepository.kt
@@ -0,0 +1,10 @@
+package com.kobietka.social_fitness_app.domain.repository.remote
+
+import com.kobietka.social_fitness_app.network.request.CreateGroupRequest
+import com.kobietka.social_fitness_app.network.response.GetGroupResponse
+import com.kobietka.social_fitness_app.util.Result
+
+
+interface GroupRemoteRepository {
+    suspend fun createGroup(createGroupRequest: CreateGroupRequest): Result<GetGroupResponse>
+}
---
+++ app/src/main/java/com/kobietka/social_fitness_app/network/repository/GroupRemoteRepositoryImpl.kt
@@ -0,0 +1,38 @@
+package com.kobietka.social_fitness_app.network.repository
+
+import com.google.gson.Gson
+import com.kobietka.social_fitness_app.domain.repository.remote.GroupRemoteRepository
+import com.kobietka.social_fitness_app.domain.service.GroupService
+import com.kobietka.social_fitness_app.network.request.CreateGroupRequest
+import com.kobietka.social_fitness_app.network.response.GetGroupResponse
+import com.kobietka.social_fitness_app.network.response.InvalidFieldErrorResponse
+import com.kobietka.social_fitness_app.util.Result
+import retrofit2.HttpException
+import java.io.IOException
+
+
+class GroupRemoteRepositoryImpl(private val groupService: GroupService) : GroupRemoteRepository {
+    override suspend fun createGroup(createGroupRequest: CreateGroupRequest): Result<GetGroupResponse> {
+        return try {
+            val response = groupService.createGroup(createGroupRequest = createGroupRequest)
+            Result.Success(data = response)
+        } catch (exception: IOException){
+            Result.Failure(message = "Cannot connect. Check your internet connection.")
+        } catch (exception: HttpException){
+            return when(exception.code()){
+                401 -> Result.Unauthorized()
+                422 -> {
+                    val responseString = exception.response()?.errorBody()?.string()
+                    val errorResponse = Gson().fromJson(responseString, InvalidFieldErrorResponse::class.java)
+                    val message = errorResponse.violations.foldRight(initial = ""){ violation, acc ->
+                        acc + violation.message + "\n"
+                    }
+                    Result.Failure(message = message)
+                }
+                else -> {
+                    Result.Failure(message = "Something went wrong. Try again later.")
+                }
+            }
+        }
+    }
+}
