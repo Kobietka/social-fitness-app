ee6fe229ed3d70a9b17725c417e12d907438aab8
kobietka <slawekgrawgry@gmail.com>
2021-10-15 10:39:55 +0200

    Add groups parameter to MainScreenState

--- app/src/main/java/com/kobietka/social_fitness_app/presentation/main_screen/MainScreenState.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/presentation/main_screen/MainScreenState.kt
@@ -1,9 +1,11 @@
 package com.kobietka.social_fitness_app.presentation.main_screen
 
+import com.kobietka.social_fitness_app.domain.model.Group
 import com.kobietka.social_fitness_app.domain.model.User
 
 data class MainScreenState(
     val user: User = User(id = "", nickname = "", email = ""),
+    val groups: List<Group> = emptyList(),
     val isCreatingGroup: Boolean = false,
     val isLoading: Boolean = false,
     val error: String = ""
