34e6d50d473671a4e9de32d5123e1e52b4bd8914
kobietka <slawekgrawgry@gmail.com>
2021-10-15 08:54:09 +0200

    Add daos to database

--- app/src/main/java/com/kobietka/social_fitness_app/data/AppDatabase.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/data/AppDatabase.kt
@@ -2,10 +2,23 @@ package com.kobietka.social_fitness_app.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.kobietka.social_fitness_app.data.dao.GroupDao
+import com.kobietka.social_fitness_app.data.dao.GroupMemberDao
 import com.kobietka.social_fitness_app.data.dao.UserCredentialsDao
+import com.kobietka.social_fitness_app.data.entity.GroupEntity
+import com.kobietka.social_fitness_app.data.entity.GroupMemberEntity
 import com.kobietka.social_fitness_app.data.entity.UserCredentialsEntity
 
-@Database(entities = [UserCredentialsEntity::class], version = 1)
+@Database(
+    entities = [
+        UserCredentialsEntity::class,
+        GroupEntity::class,
+        GroupMemberEntity::class
+    ],
+    version = 1
+)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userCredentialsDao(): UserCredentialsDao
+    abstract fun groupDao(): GroupDao
+    abstract fun groupMemberDao(): GroupMemberDao
 }
