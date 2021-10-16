a9681a32d77448e9b8d241c85dce771b01c5c4e3
kobietka <slawekgrawgry@gmail.com>
2021-10-16 09:29:55 +0200

    Add on conflict strategy to GroupDao

--- app/src/main/java/com/kobietka/social_fitness_app/data/dao/GroupDao.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/data/dao/GroupDao.kt
@@ -2,6 +2,7 @@ package com.kobietka.social_fitness_app.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
 import com.kobietka.social_fitness_app.data.entity.GroupEntity
 import kotlinx.coroutines.flow.Flow
@@ -10,7 +11,7 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface GroupDao {
 
-    @Insert
+    @Insert(onConflict = REPLACE)
     suspend fun insert(groupEntity: GroupEntity)
 
     @Query("select * from `group`")
