b472a1ccdbe92f567210f2d0a5de0a467a636a56
kobietka <slawekgrawgry@gmail.com>
2021-10-16 11:11:01 +0200

    Add editGroup method to GroupService

--- app/src/main/java/com/kobietka/social_fitness_app/domain/service/GroupService.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/domain/service/GroupService.kt
@@ -1,6 +1,7 @@
 package com.kobietka.social_fitness_app.domain.service
 
 import com.kobietka.social_fitness_app.network.request.CreateGroupRequest
+import com.kobietka.social_fitness_app.network.request.EditGroupRequest
 import com.kobietka.social_fitness_app.network.response.GetGroupResponse
 import retrofit2.http.*
 
@@ -15,6 +16,12 @@ interface GroupService {
 
     @GET("/api/group/{id}")
     suspend fun getGroup(groupId: String): GetGroupResponse
+
+    @PATCH("/api/group/{id}")
+    suspend fun editGroup(
+        @Path("id") groupId: String,
+        @Body editGroupRequest: EditGroupRequest
+    ): GetGroupResponse
 
     @DELETE("/api/group/{id}")
     suspend fun deleteGroup(@Path("id") groupId: String)
