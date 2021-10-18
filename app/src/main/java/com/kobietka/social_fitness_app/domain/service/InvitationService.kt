c2337e83cb5ee918eeda500804aa1e6d974cdc09
kobietka <slawekgrawgry@gmail.com>
2021-10-18 12:55:27 +0200

    Add deleteInvitation method to InvitationService

--- app/src/main/java/com/kobietka/social_fitness_app/domain/service/InvitationService.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/domain/service/InvitationService.kt
@@ -3,12 +3,17 @@ package com.kobietka.social_fitness_app.domain.service
 import com.kobietka.social_fitness_app.network.request.CreateInvitationRequest
 import com.kobietka.social_fitness_app.network.response.InvitationResponse
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 
 interface InvitationService {
 
     @POST("/api/invitation")
     suspend fun createInvitation(@Body createInvitationRequest: CreateInvitationRequest): InvitationResponse
+
+    @DELETE("/api/invitation/{id}")
+    suspend fun deleteInvitation(@Path("id") id: String)
 
 }
