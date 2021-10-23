bc298727539bac9291d9c42f764e9c632b982788
kobietka <slawekgrawgry@gmail.com>
2021-10-23 11:48:14 +0200

    Add CommentDto

---
+++ app/src/main/java/com/kobietka/social_fitness_app/network/response/CommentDto.kt
@@ -0,0 +1,11 @@
+package com.kobietka.social_fitness_app.network.response
+
+import com.kobietka.social_fitness_app.domain.model.User
+
+
+data class CommentDto(
+    val id: String,
+    val content: String,
+    val user: User,
+    val createdAt: String
+)
