2efedc571ac0d189666a91116553455492966ada
kobietka <slawekgrawgry@gmail.com>
2021-10-24 09:53:57 +0200

    Add on conflict strategy to insert in PostDao

--- app/src/main/java/com/kobietka/social_fitness_app/data/dao/PostDao.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/data/dao/PostDao.kt
@@ -2,6 +2,7 @@ package com.kobietka.social_fitness_app.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
 import com.kobietka.social_fitness_app.data.entity.PostEntity
 import kotlinx.coroutines.flow.Flow
@@ -10,7 +11,7 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface PostDao {
 
-    @Insert
+    @Insert(onConflict = REPLACE)
     suspend fun insert(postEntity: PostEntity)
 
     @Query("select * from post where groupId = :groupId")
