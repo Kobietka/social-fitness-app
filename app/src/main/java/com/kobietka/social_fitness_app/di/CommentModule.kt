b08531c47727f6e4f3d1fc06f90ce25883b428cf
kobietka <slawekgrawgry@gmail.com>
2021-10-25 10:44:51 +0200

    Add EditCommentUseCase

--- app/src/main/java/com/kobietka/social_fitness_app/di/CommentModule.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/di/CommentModule.kt
@@ -3,6 +3,7 @@ package com.kobietka.social_fitness_app.di
 import com.kobietka.social_fitness_app.domain.repository.local.CommentRepository
 import com.kobietka.social_fitness_app.domain.repository.remote.CommentRemoteRepository
 import com.kobietka.social_fitness_app.domain.usecase.comment.CreateCommentUseCase
+import com.kobietka.social_fitness_app.domain.usecase.comment.EditCommentUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -24,4 +25,35 @@ class CommentModule {
         )
     }
 
+    @Provides
+    fun provideEditCommentUseCase(
+        commentRemoteRepository: CommentRemoteRepository,
+        commentRepository: CommentRepository
+    ): EditCommentUseCase {
+        return EditCommentUseCase(
+            commentRemoteRepository = commentRemoteRepository,
+            commentRepository = commentRepository
+        )
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/kobietka/social_fitness_app/domain/usecase/comment/EditCommentUseCase.kt
@@ -0,0 +1,70 @@
+package com.kobietka.social_fitness_app.domain.usecase.comment
+
+import com.kobietka.social_fitness_app.data.entity.CommentEntity
+import com.kobietka.social_fitness_app.domain.repository.local.CommentRepository
+import com.kobietka.social_fitness_app.domain.repository.remote.CommentRemoteRepository
+import com.kobietka.social_fitness_app.network.request.EditCommentRequest
+import com.kobietka.social_fitness_app.util.NetworkResult
+import com.kobietka.social_fitness_app.util.Progress
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+
+class EditCommentUseCase(
+    private val commentRemoteRepository: CommentRemoteRepository,
+    private val commentRepository: CommentRepository
+) {
+    operator fun invoke(
+        postId: String,
+        commentId: String,
+        content: String
+    ): Flow<Progress> = flow {
+        emit(Progress.Loading)
+        val result = commentRemoteRepository.editComment(
+            commentId = commentId,
+            editCommentRequest = EditCommentRequest(
+                id = commentId,
+                content = content
+            )
+        )
+        when(result){
+            is NetworkResult.Success -> {
+                result.data.let { commentDto ->
+                    commentRepository.insert(
+                        CommentEntity(
+                            id = commentDto.id,
+                            content = commentDto.content,
+                            postId = postId,
+                            createdAt = commentDto.createdAt,
+                            userId = commentDto.user.id
+                        )
+                    )
+                    emit(Progress.Finished)
+                }
+            }
+            is NetworkResult.Failure -> emit(Progress.Error(message = result.message))
+            is NetworkResult.Unauthorized -> emit(Progress.Unauthorized)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
