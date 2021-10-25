585a3caf10f7af895656618ad3c54e5409128775
kobietka <slawekgrawgry@gmail.com>
2021-10-25 11:06:36 +0200

    Add ActivityDto

---
+++ app/src/main/java/com/kobietka/social_fitness_app/network/response/ActivityDto.kt
@@ -0,0 +1,10 @@
+package com.kobietka.social_fitness_app.network.response
+
+
+
+data class ActivityDto(
+    val id: String,
+    val name: String,
+    val value: Int,
+    val createdAt: String
+)
