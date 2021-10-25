46bafa1a1d75e6b1c705ee4507b9d72b8fc3e96a
kobietka <slawekgrawgry@gmail.com>
2021-10-25 11:07:05 +0200

    Add EditEventRequest

---
+++ app/src/main/java/com/kobietka/social_fitness_app/network/request/EditEventRequest.kt
@@ -0,0 +1,15 @@
+package com.kobietka.social_fitness_app.network.request
+
+
+
+data class EditEventRequest(
+    val id: String,
+    val name: String,
+    val description: String,
+    val pointGoal: Int,
+    val pointPerRep: Int,
+    val pointPerMinute: Int,
+    val startDate: String,
+    val endDate: String,
+    val eventType: String
+)
