977a099819bf6a6a1f0cd26cceb033f3c8a6b85e
kobietka <slawekgrawgry@gmail.com>
2021-10-25 10:11:27 +0200

    Add foreign key to PostEntity

--- app/src/main/java/com/kobietka/social_fitness_app/data/entity/PostEntity.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/data/entity/PostEntity.kt
@@ -2,10 +2,23 @@ package com.kobietka.social_fitness_app.data.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
 import androidx.room.PrimaryKey
 
 
-@Entity(tableName = "post")
+@Entity(
+    tableName = "post",
+    foreignKeys = [
+        ForeignKey(
+            entity = GroupEntity::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("groupId"),
+            onDelete = CASCADE,
+            onUpdate = CASCADE
+        )
+    ]
+)
 data class PostEntity(
     @PrimaryKey val id: String,
     @ColumnInfo val groupId: String,
