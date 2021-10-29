46dbd70abbd4019122f7763af592d2c88d53a0c5
kobietka <slawek.krupa.dev@gmail.com>
2021-10-29 12:01:10 +0200

    Add EditGroup to Screen

--- app/src/main/java/com/kobietka/social_fitness_app/presentation/Screen.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/presentation/Screen.kt
@@ -6,6 +6,7 @@ sealed class Screen(val route: String){
     object Login : Screen("/login")
     object MainScreen : Screen("/main")
     object Group : Screen("/group/{groupId}")
+    object EditGroup : Screen("/edit_group/{groupId}")
     object EditUser : Screen("/edit_user")
     object Loading : Screen("/loading")
 }
