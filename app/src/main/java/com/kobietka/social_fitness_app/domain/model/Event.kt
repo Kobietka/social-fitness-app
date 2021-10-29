a6e7b522116197b61be6bd38d985c4d225a253fd
kobietka <slawek.krupa.dev@gmail.com>
2021-10-29 10:41:51 +0200

    Add Event

---
+++ app/src/main/java/com/kobietka/social_fitness_app/domain/model/Event.kt
@@ -0,0 +1,15 @@
+package com.kobietka.social_fitness_app.domain.model
+
+
+
+data class Event(
+    val id: String,
+    val name: String,
+    val description: String,
+    val pointGoal: Int,
+    val pointPerMinute: Int,
+    val pointPerRep: Int,
+    val startDate: String,
+    val endDate: String,
+    val eventType: EventType
+)
