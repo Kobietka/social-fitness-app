0a15565c86f266998a69dbf219125e98d605953b
kobietka <slawek.krupa.dev@gmail.com>
2021-10-29 09:03:14 +0200

    Add on conflict strategy to ActivityDao

--- app/src/main/java/com/kobietka/social_fitness_app/data/dao/ActivityDao.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/data/dao/ActivityDao.kt
@@ -2,6 +2,7 @@ package com.kobietka.social_fitness_app.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
 import com.kobietka.social_fitness_app.data.entity.ActivityEntity
 import kotlinx.coroutines.flow.Flow
@@ -10,7 +11,7 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface ActivityDao {
 
-    @Insert
+    @Insert(onConflict = REPLACE)
     suspend fun insert(activityEntity: ActivityEntity)
 
     @Query("select * from activity where eventId = :eventId")
