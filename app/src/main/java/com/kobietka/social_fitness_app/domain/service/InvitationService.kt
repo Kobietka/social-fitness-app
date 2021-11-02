56fd7a1718ba96454b73354a60bfd2a41e3b15fc
kobietka <slawek.krupa.dev@gmail.com>
2021-11-02 10:38:33 +0100

    Add return type to deleteInvitation method

--- app/src/main/java/com/kobietka/social_fitness_app/domain/service/InvitationService.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/domain/service/InvitationService.kt
@@ -2,6 +2,7 @@ package com.kobietka.social_fitness_app.domain.service
 
 import com.kobietka.social_fitness_app.network.request.CreateInvitationRequest
 import com.kobietka.social_fitness_app.network.response.InvitationResponse
+import retrofit2.Response
 import retrofit2.http.*
 
 
@@ -11,6 +12,6 @@ interface InvitationService {
     suspend fun createInvitation(@Body createInvitationRequest: CreateInvitationRequest): InvitationResponse
 
     @DELETE("/api/invitation/{id}")
-    suspend fun deleteInvitation(@Path("id") id: String)
+    suspend fun deleteInvitation(@Path("id") id: String): Response<Unit>
 
 }
