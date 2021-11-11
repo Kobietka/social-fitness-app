567e876edba38278d696e9c82b2c9909e88c917c
kobietka <slawek.krupa.dev@gmail.com>
2021-11-11 13:20:44 +0100

    Add parameters to PostScreenState

--- app/src/main/java/com/kobietka/social_fitness_app/presentation/post/PostScreenState.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/presentation/post/PostScreenState.kt
@@ -7,8 +7,11 @@ import com.kobietka.social_fitness_app.domain.model.Post
 
 data class PostScreenState(
     val isLoading: Boolean = false,
+    val isEditingPost: Boolean = false,
+    val isDeletePostLoading: Boolean = false,
     val loadingError: String = "",
-    val post: Post = Post(id = "", content = "", user = GroupMember(id = "", nickname = ""), createdAt = ""),
+    val loggedUser: GroupMember = GroupMember(id = "", userId = "", nickname = ""),
+    val post: Post = Post(id = "", content = "", user = GroupMember(id = "", userId = "", nickname = ""), createdAt = ""),
     val comments: List<Comment> = emptyList(),
     val isCreatingComment: Boolean = false,
     val creatingCommentError: String = ""
