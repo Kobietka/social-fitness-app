3e4e3af1f527443d3ce602f8bef2020cc64f9e7c
kobietka <slawek.krupa.dev@gmail.com>
2021-11-15 10:32:42 +0100

    Add CreateEventScreenState

---
+++ app/src/main/java/com/kobietka/social_fitness_app/presentation/create_event/CreateEventScreenState.kt
@@ -0,0 +1,8 @@
+package com.kobietka.social_fitness_app.presentation.create_event
+
+
+
+data class CreateEventScreenState(
+    val isLoading: Boolean = false,
+    val createEventError: String = ""
+)
