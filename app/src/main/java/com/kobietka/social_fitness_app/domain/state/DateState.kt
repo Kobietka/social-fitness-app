d16814a48298cd8bb4cb1e1321386cd7086fdee3
kobietka <slawek.krupa.dev@gmail.com>
2021-11-17 10:14:54 +0100

    Add error property to DateState and EventTypeState

--- app/src/main/java/com/kobietka/social_fitness_app/domain/state/DateState.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/domain/state/DateState.kt
@@ -3,5 +3,6 @@ package com.kobietka.social_fitness_app.domain.state
 
 data class DateState(
     val instantString: String? = null,
-    val formatted: String? = null
+    val formatted: String? = null,
+    val error: String = ""
 )
--- app/src/main/java/com/kobietka/social_fitness_app/domain/state/EventTypeState.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/domain/state/EventTypeState.kt
@@ -3,6 +3,7 @@ package com.kobietka.social_fitness_app.domain.state
 
 
 data class EventTypeState(
-    val name: String,
-    val code: String
+    val name: String? = null,
+    val code: String? = null,
+    val error: String = ""
 )
