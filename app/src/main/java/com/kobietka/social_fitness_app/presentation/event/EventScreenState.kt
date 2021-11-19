99d65de6b7771ddc221bcc4ae534f0afacb9a527
kobietka <slawek.krupa.dev@gmail.com>
2021-11-19 10:33:02 +0100

    Add eventMembers property to EventScreenState

--- app/src/main/java/com/kobietka/social_fitness_app/presentation/event/EventScreenState.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/presentation/event/EventScreenState.kt
@@ -1,10 +1,12 @@
 package com.kobietka.social_fitness_app.presentation.event
 
 import com.kobietka.social_fitness_app.domain.model.Event
+import com.kobietka.social_fitness_app.domain.model.EventMember
 import com.kobietka.social_fitness_app.domain.model.User
 
 data class EventScreenState(
     val loggedUser: User? = null,
     val event: Event? = null,
-    val isUserAGroupOwner: Boolean = false
+    val isUserAGroupOwner: Boolean = false,
+    val eventMembers: List<EventMember> = emptyList()
 )
