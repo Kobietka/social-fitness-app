6ebf84a64128b4018b2cc9c456dcc9317d19a049
kobietka <slawek.krupa.dev@gmail.com>
2021-11-21 09:19:21 +0100

    Add color

--- app/src/main/java/com/kobietka/social_fitness_app/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/kobietka/social_fitness_app/presentation/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.kobietka.social_fitness_app.presentation.theme
+package com.kobietka.social_fitness_app.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
@@ -6,3 +6,5 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val gray = Color(0xFFEEEEEE)
