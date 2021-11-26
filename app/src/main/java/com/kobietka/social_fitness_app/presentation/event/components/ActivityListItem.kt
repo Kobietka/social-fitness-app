e27844ae305fb9ef25349f39a9730133a8b16129
kobietka <slawek.krupa.dev@gmail.com>
2021-11-26 12:24:42 +0100

    Add ActivityListItem

---
+++ app/src/main/java/com/kobietka/social_fitness_app/presentation/event/components/ActivityListItem.kt
@@ -0,0 +1,66 @@
+package com.kobietka.social_fitness_app.presentation.event.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.kobietka.social_fitness_app.domain.model.Activity
+import com.kobietka.social_fitness_app.domain.model.EventType
+
+
+@Composable
+fun ActivityListItem(
+    activity: Activity,
+    eventType: EventType
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 3.dp, bottom = 3.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column {
+            Text(
+                text = activity.name,
+                fontWeight = FontWeight.Medium
+            )
+            Text(
+                text = activity.createdAt,
+                color = Color.Gray
+            )
+            Text(
+                text = when(eventType){
+                    EventType.REPETITION -> activity.value.toString() + "repetitions"
+                    EventType.TIME -> {
+                        val minutes = activity.value / 60
+                        val seconds = activity.value - minutes * 60
+                        "$minutes ${if(minutes == 1) "minute" else "minutes"} $seconds ${if(seconds == 1) "second" else "seconds"}"
+                    }
+                    EventType.LESS_TIME -> {
+                        val minutes = activity.value / 60
+                        val seconds = activity.value - minutes * 60
+                        "$minutes ${if(minutes == 1) "minute" else "minutes"} $seconds ${if(seconds == 1) "second" else "seconds"}"
+                    }
+                    else -> ""
+                }
+            )
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
